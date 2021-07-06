66ad078b6e1a8fe09105191f12fa2a540229d175
Zac Garby <me@zacgarby.co.uk>
2021-07-06 18:14:01 +0100

    registration form web server

--- registration/go.mod
+++ registration/go.mod
@@ -1,3 +1,5 @@
 module hacksocnotts.co.uk/voting/registration
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- registration/main.go
+++ registration/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("registration server")
+	r := mux.NewRouter()
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/index.html")
+	})
+
+	log.Println("starting registration server on :10000")
+	log.Fatal(http.ListenAndServe(":10000", r))
 }
